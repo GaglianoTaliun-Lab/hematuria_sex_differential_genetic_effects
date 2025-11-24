c90d89f6e789750889936a9ee42f600e90a51788
Frida Lona Durazo <32912529+ape4fld@users.noreply.github.com>
2025-11-24 15:09:05 -0500

    Update format_datasets_BPtraits.R

--- scripts/hyprcoloc/format_datasets_BPtraits.R
+++ scripts/hyprcoloc/format_datasets_BPtraits.R
@@ -12,13 +12,14 @@ library(stringr)
 library(dplyr)
 library(tidyr)
 library(data.table)
-library(colochelpR)
+library(BSgenome)
+library(GenomicRanges)
 library(SNPlocs.Hsapiens.dbSNP144.GRCh37)
 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-home_dir="/home/fridald4/projects/def-gsarah"
-project_dir="/home/fridald4/projects/def-gsarah/fridald4/col4a2_hematuria"
+home_dir="/home/fridald4/links/projects/def-gsarah"
+project_dir="/home/fridald4/links/projects/def-gsarah/fridald4/col4a2_hematuria"
 dbsnp_144 <- SNPlocs.Hsapiens.dbSNP144.GRCh37
 out_ext=".txt"
 
@@ -28,11 +29,13 @@ flanking_kb=100
 min_bp = 110801310-(flanking_kb*1000)
 max_bp = 111165556+(flanking_kb*1000)
 
+source("/scratch/fridald4/col4a2_hematuria/convert_loc_to_rs.R")
+
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 #                                               | female DBP |
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-# sample size: 174,664
+sample size: 174,664
 
 file_path <- file.path(project_dir, "datasets", 
     "GCST90301699.h.DBP_females.tsv.gz")
@@ -110,3 +113,84 @@ PP_f <- PP_f %>%
 
 fwrite(PP_f, file = here(project_dir, "data_for_coloc",stringr::str_c("PP_females", out_ext)), sep = "\t")
 
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#                                               | male DBP |
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+# sample size: 174,664
+
+file_path <- file.path(project_dir, "datasets", 
+    "GCST90301700.h.DBP_males.tsv.gz")
+fread(file_path, nrows = 6)
+
+DBP_m <- fread(file_path) %>%
+    separate(data = ., col = pos.hg19, into = c("CHR", "BP"), sep = ":") %>%
+    filter(., CHR == 13 & BP >= min_bp & BP <= max_bp) %>%
+    convert_loc_to_rs(., dbsnp_144) %>%
+    dplyr::mutate(varbeta = standard_error^2,
+                  MAF = case_when(
+                    effect_allele_frequency >= 0.5 ~ 1-effect_allele_frequency,
+                    effect_allele_frequency < 0.5 ~ effect_allele_frequency
+                  ),
+                  N = 174664) %>%
+    dplyr::select(
+      CHR,
+      BP,
+      effect_allele,
+      other_allele,
+      SNP,
+      beta,
+      varbeta,
+      pvalues = p_value,
+      MAF,
+      N)
+
+DBP_m <- DBP_m %>%
+  dplyr::mutate(CHR_BP = stringr::str_c(CHR, ":", BP)) %>%
+  dplyr::group_by(CHR_BP) %>%
+  dplyr::filter(!any(row_number() > 1))  %>%
+  dplyr::ungroup() %>%
+  dplyr::select(-CHR_BP)
+
+fwrite(DBP_m, file = here(project_dir, "data_for_coloc",stringr::str_c("DBP_males", out_ext)), sep = "\t")
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#                                               | male PP |
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+# sample size: 174,664
+
+file_path <- file.path(project_dir, "datasets", 
+    "GCST90301703.h.PP_males.tsv.gz")
+fread(file_path, nrows = 6)
+
+PP_m <- fread(file_path) %>%
+    separate(., pos.hg19, c("CHR", "BP"), sep = ":") %>%
+    filter(., CHR == 13 & BP >= min_bp & BP <= max_bp) %>%
+    convert_loc_to_rs(., dbsnp_144) %>%
+    dplyr::mutate(varbeta = standard_error^2,
+                  MAF = case_when(
+                    effect_allele_frequency >= 0.5 ~ 1-effect_allele_frequency,
+                    effect_allele_frequency < 0.5 ~ effect_allele_frequency
+                  ),
+                  N = 174664) %>%
+    dplyr::select(
+      CHR,
+      BP,
+      effect_allele,
+      other_allele,
+      SNP,
+      beta,
+      varbeta,
+      pvalues = p_value,
+      MAF,
+      N)
+
+PP_m <- PP_m %>%
+  dplyr::mutate(CHR_BP = stringr::str_c(CHR, ":", BP)) %>%
+  dplyr::group_by(CHR_BP) %>%
+  dplyr::filter(!any(row_number() > 1))  %>%
+  dplyr::ungroup() %>%
+  dplyr::select(-CHR_BP)
+
+fwrite(PP_m, file = here(project_dir, "data_for_coloc",stringr::str_c("PP_males", out_ext)), sep = "\t")
